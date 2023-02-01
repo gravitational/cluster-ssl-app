15b33549f938057c750494af0dec4328956b78ff
Sergei Antipov <s.antipov@mulesoft.com>
2023-02-01 14:30:05 -0500

    [W-12432637] Helm chart for local RKE2 cluster

--- Makefile
+++ Makefile
@@ -5,6 +5,8 @@ endif
 
 TELE ?= $(shell which tele)
 GRAVITY ?= $(shell which gravity)
+GRAVITY_VERSION ?= 8.0.10
+DOCKER_REPOSITORY_PREFIX ?= "172.28.128.101:30050"
 
 REPOSITORY := gravitational.io
 NAME := cluster-ssl-app
@@ -37,6 +39,7 @@ IMPORT_OPTIONS := --vendor \
 BUILD_DIR := build
 BINARIES_DIR := bin
 TARBALL := $(BUILD_DIR)/application.tar
+UNAME := $(shell uname | tr A-Z a-z)
 
 $(STATEDIR):
 	mkdir -p $(STATEDIR)
@@ -51,6 +54,20 @@ what-version:
 .PHONY: images
 images:
 	cd $(PWD)/images &&	$(MAKE) VERSION=$(VERSION) && cd $(PWD)
+
+.PHONY: docker-push
+docker-push:
+	docker tag cluster-ssl-hook:$(VERSION) $(DOCKER_REPOSITORY_PREFIX)/cluster-ssl-hook:$(VERSION)
+	docker push $(DOCKER_REPOSITORY_PREFIX)/cluster-ssl-hook:$(VERSION)
+
+.PHONY: helm-install
+helm-install:
+	helm --kubeconfig ~/go/src/github.com/mulesoft/rke2-playground/ansible/kubeconfig.yaml install cluster-ssl resources/chart \
+		--values resources/custom-values.yaml --set "image.tag=$(VERSION)" --set "image.repository=$(DOCKER_REPOSITORY_PREFIX)/cluster-ssl-hook"
+
+.PHONY: helm-uninstall
+helm-uninstall:
+	helm --kubeconfig ~/go/src/github.com/mulesoft/rke2-playground/ansible/kubeconfig.yaml uninstall cluster-ssl
 
 .PHONY: build-app
 build-app: images
@@ -74,11 +91,9 @@ $(BINARIES_DIR):
 
 .PHONY: download-binaries
 download-binaries: $(BINARIES_DIR)
-	for name in gravity tele; \
-	do \
-		curl https://get.gravitational.io/telekube/bin/$(GRAVITY_VERSION)/linux/x86_64/$$name -o $(BINARIES_DIR)/$$name; \
-		chmod +x $(BINARIES_DIR)/$$name; \
-	done
+	aws s3 cp --only-show-errors s3://gravity-installers/gravity/release/$(GRAVITY_VERSION)/$(UNAME)-amd64/tele $(BINARIES_DIR)/tele
+	aws s3 cp --only-show-errors s3://gravity-installers/gravity/release/$(GRAVITY_VERSION)/$(UNAME)-amd64/gravity $(BINARIES_DIR)/gravity
+	chmod +x $(BINARIES_DIR)/gravity $(BINARIES_DIR)/tele
 
 .PHONY: clean
 clean: clean-state-dir
--- images/hook/hook.sh
+++ images/hook/hook.sh
@@ -1,32 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -xe
 
 function create_ca_secret {
-	cfssl gencert -initca ca-csr.json|cfssljson -bare ca -
+	cfssl gencert -initca ca-csr.json | cfssljson -bare ca -
 
 	kubectl create secret generic cluster-ca \
-			--from-file=ca.pem=ca.pem \
-			--from-file=ca-key=ca-key.pem \
-			--from-file=ca.csr=ca.csr
+		--from-file=ca.pem=ca.pem \
+		--from-file=ca-key=ca-key.pem \
+		--from-file=ca.csr=ca.csr
 }
 
 function create_certificate_secrets {
-	if kubectl get secret/cluster-ca ; then
+	if kubectl get secret/cluster-ca; then
 		echo "secret/cluster-ca already exists"
-        kubectl get secret cluster-ca -o json | jq -r '.data."ca.pem"' | base64 -d > ca.pem
-        kubectl get secret cluster-ca -o json | jq -r '.data."ca-key"' | base64 -d > ca-key.pem
+		kubectl get secret cluster-ca -o json | jq -r '.data."ca.pem"' | base64 -d >ca.pem
+		kubectl get secret cluster-ca -o json | jq -r '.data."ca-key"' | base64 -d >ca-key.pem
 	else
-        create_ca_secret
+		create_ca_secret
 	fi
 
-	if kubectl get secret/cluster-default-ssl ; then
+	if kubectl get secret/cluster-default-ssl; then
 		echo "secret/cluster-default-ssl already exists"
 	else
 		cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json \
-			  -profile=server default-server-csr.json | cfssljson -bare default-server
+			-profile=server default-server-csr.json | cfssljson -bare default-server
 		cp default-server.pem default-server-with-chain.pem
-		cat ca.pem >> default-server-with-chain.pem
+		cat ca.pem >>default-server-with-chain.pem
 
 		kubectl create secret generic cluster-default-ssl \
 			--from-file=default-server.pem=default-server.pem \
@@ -36,27 +36,10 @@ function create_certificate_secrets {
 	fi
 }
 
-cd /root/cfssl
+create_certificate_secrets
 
-if [[ $1 = "install" ]]; then
-    create_certificate_secrets
-
-elif [[ $1 = "update" ]]; then
-    kubectl delete secret/cluster-ca --namespace=default --ignore-not-found
-    kubectl delete secret/cluster-default-ssl --namespace=default --ignore-not-found
-    kubectl delete secret/cluster-kube-system-ssl --namespace=default --ignore-not-found
-    create_certificate_secrets
-
-elif [[ $1 = "uninstall" ]]; then
-
-	for sname in cluster-ca cluster-default-ssl cluster-kube-system-ssl
-	do
-		kubectl delete secret $sname --ignore-not-found
-	done
-
-else
-
-	echo "Missing argument, should be 'install', 'update' or 'uninstall'"
-	exit 1
-
-fi
+# elif [[ $1 = "update" ]]; then
+#     kubectl delete secret/cluster-ca --namespace=default --ignore-not-found
+#     kubectl delete secret/cluster-default-ssl --namespace=default --ignore-not-found
+#     kubectl delete secret/cluster-kube-system-ssl --namespace=default --ignore-not-found
+#     create_certificate_secrets
